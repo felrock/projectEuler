854fbf0a03d5a0375852a28474a0d9198eed7456
Felix <stenbackfelix@gmail.com>
2020-09-21 00:12:05 +0200

    added djikstra algortihm solution for both 82, 83 becuase they were very similar

---
+++ problem83.cpp
@@ -0,0 +1,150 @@
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define INF 99999999
+
+
+using namespace std;
+
+
+struct Node
+{
+    int index;
+    int weight;
+    vector<int> neighbors;
+};
+
+vector<int> parseLine(string line, char delimiter)
+{
+    string temp;
+    vector<int> result;
+    for(const auto& ch : line)
+    {
+        if(ch == delimiter)
+        {
+            result.push_back(stoi(temp));
+            temp = "";
+        }
+        else
+        {
+            temp += ch;
+        }
+    }
+
+    // add what left when loop exits
+    result.push_back(stoi(temp));
+
+    return result;
+}
+
+int minDistance(vector<int> dist, vector<bool> visited)
+{
+    int md = INF, index=0;
+    for(int i=0; i < dist.size(); ++i)
+    {
+        if(!visited[i] && dist[i] <= md)
+        {
+            md = dist[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+bool allVisted(vector<bool> visited)
+{
+    for( auto v : visited)
+    {
+        if(!v)
+            return true;
+    }
+    return false;
+}
+
+int main(int argc, char** argv)
+{
+    vector<vector<int>> grid;
+
+    // read input into 2d matrix
+    string file_name = "problem83.txt"; 
+    fstream fs(file_name);
+    for(string line; getline(fs, line);)
+    {
+        grid.push_back(parseLine(line, ','));    
+    }
+    // create graph
+    // There are a couple of ways to store a graph in memory, one way is to
+    // store all vertexes to each node as arrays. Meaning, for 80 nodes, 
+    // 80x80 arrays are used to represent it.
+    //
+    // Another way is to store nodes in a linked-list fashion. I will use the latter.
+
+    vector<Node*> nodes;
+    int width = grid[0].size();
+    int height = grid.size();
+    int node_count = width*height;
+
+    for(int i=0; i < height; ++i)
+    {
+        vector<Node*> nodes_row;
+        for(int j=0; j < width; ++j)
+        {
+           Node *temp = new Node;
+           temp->index = i*height + j;
+           temp->weight = grid[i][j];
+           vector<int> nb;
+           if(i+1 < height)
+           {
+                nb.push_back((i+1)*height + j);
+           }
+           if(i-1 >= 0)
+           {
+                nb.push_back((i-1)*height + j);
+           }
+           if(j+1 < width)
+           {
+                nb.push_back(i*height + (j+1));
+           }
+           if(j-1 >= 0)
+           {
+                nb.push_back(i*height + (j-1));
+           }
+           temp->neighbors = nb;
+           nodes.push_back(temp);
+        }
+    }
+    // Run djikstras algo from all possible start positions, left side
+    int best_path_dist = INF;
+    vector<int> new_dist;
+    vector<bool> new_visited;
+    for(int i=0; i < node_count; ++i)
+    {
+        new_dist.push_back(INF);
+        new_visited.push_back(false);
+    }
+    vector<int> dist = new_dist;
+    vector<bool> visited = new_visited;
+    
+    dist[0] = nodes[0]->weight;
+
+    while(allVisted(visited))
+    {
+        
+        int j = minDistance(dist, visited);
+        visited[j] = true;
+        for(auto k : nodes[j]->neighbors)
+        {
+            if(!visited[k] && dist[j] != INF && 
+               (dist[j] + nodes[k]->weight) < dist[k])
+            {
+                dist[k] = dist[j] + nodes[k]->weight;
+            }
+        }
+
+    }
+    cout << "Best found path distance is: " << dist[node_count-1] << endl;
+    return EXIT_SUCCESS;
+}
